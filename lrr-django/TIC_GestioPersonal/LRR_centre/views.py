f96f53c90e675179639bb8768bfdbe3baea262e5
Laia Rodriguez Ramos <lrodrigu22@jaumebalmes.net>
2024-04-16 19:37:49 +0200

    Feta practica 0, falta video.

--- lrr-django/TIC_GestioPersonal/LRR_centre/views.py
+++ lrr-django/TIC_GestioPersonal/LRR_centre/views.py
@@ -7,3 +7,10 @@ from django.template import loader
 
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def home(request):
+    professor = {"nom": "Oriol", "cognom": "Roca", "edat": 25}
+    return render(request, 'home.html', {'professor': professor})
