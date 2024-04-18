9291e22daaa061533b467b5cd1293db2f624cf73
Laia Rodriguez Ramos <lrodrigu22@jaumebalmes.net>
2024-04-18 19:19:55 +0200

    Ara vaig a preparar el db.

--- lrr-django/TIC_GestioPersonal/LRR_centre/views.py
+++ lrr-django/TIC_GestioPersonal/LRR_centre/views.py
@@ -14,3 +14,11 @@ def about(request):
 def index(request):
     professor = {"nom": "Oscar", "cognom": "Rovira", "edat": 30}
     return render(request, 'index.html', {'professor': professor})
+
+def student(request):
+    alumnat = {"nom": "Oscar", "cognom": "Rovira", "edat": 30}
+    return render(request, 'student.html', {'alumnat': alumnat})
+
+def teacher(request):
+    professor = {"nom": "Oscar", "cognom": "Rovira", "edat": 30}
+    return render(request, 'teacher.html', {'professor': professor})
